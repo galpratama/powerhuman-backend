754f6b57ae3a5c7eef8ca0656239dcb33c5ab4e2
Galih Pratama <hanamura.iost@gmail.com>
2022-07-05 20:36:03 +0700

    first commit

---
+++ database/migrations/2022_07_05_125925_create_responsibilities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responsibilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('role_id')->unsigned();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responsibilities');
+    }
+};
---
+++ database/migrations/2022_07_05_125937_create_employees_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('gender')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+
+            $table->bigInteger('team_id')->unsigned();
+            $table->bigInteger('role_id')->unsigned();
+
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('verified_at')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
