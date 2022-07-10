ee7ff71256f19eb779bcaec68b6b81a9d69b6c00
Shelly Sinaga <shelly.sinaga@gmail.com>
2022-07-11 03:42:26 +0700

    feat: separate pagination from data to improve response accessibility and readability

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -29,18 +29,23 @@ class ResponseFormatter
         self::$response['meta']['message'] = $message;
         self::$response['data'] = $data;
 
+        if ($data->current_page) {
+            self::$response['data'] = $data->data;
+            unset($data->data);
+            self::$response['pagination'] = $data;
+        };
+
         return response()->json(self::$response, self::$response['meta']['code']);
     }
 
     /**
      * Give error response.
      */
-    public static function error($data = null, $message = null, $code = 400)
+    public static function error($message = null, $code = 400)
     {
         self::$response['meta']['status'] = 'error';
         self::$response['meta']['code'] = $code;
         self::$response['meta']['message'] = $message;
-        self::$response['data'] = $data;
 
         return response()->json(self::$response, self::$response['meta']['code']);
     }
