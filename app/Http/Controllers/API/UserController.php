016a6e311bc692f816a358381a740c6d99712ed5
Galih Pratama <hanamura.iost@gmail.com>
2022-07-21 14:01:07 +0700

    Update pertemuan 8

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Rules\Password;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            // Validate request
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            // Find user by email
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password)) {
+                throw new Exception('Invalid password');
+            }
+
+            // Generate token
+            $toketResult = $user->createToken('authToken')->plainTextToken;
+
+            // Return response
+            return ResponseFormatter::success([
+                'access_token' => $toketResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Login success');
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage());
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            // Validate request
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', new Password],
+            ]);
+
+            // Create user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            // Generate token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            // Return response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Register success');
+        } catch (Exception $error) {
+            // Return error response
+            return ResponseFormatter::error($error->getMessage());
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        // Revoke Token
+        $token = $request->user()->currentAccessToken()->delete();
+
+        // Return response
+        return ResponseFormatter::success($token, 'Logout success');
+    }
+
+    public function fetch(Request $request)
+    {
+        // Get user
+        $user = $request->user();
+
+        // Return response
+        return ResponseFormatter::success($user, 'Fetch success');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/company', [CompanyController::class, 'all']);
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('user', [UserController::class, 'fetch'])->middleware('auth:sanctum');
